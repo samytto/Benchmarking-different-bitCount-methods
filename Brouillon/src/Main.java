d56732326a6da79b6270426b54d8660c78e236b8
CHAMBI Samy <CHAMBI.SAMY@GMAIL.COM>
2014-09-04 12:35:47 -0400

    Adding a dry run and improving the code

--- Brouillon/src/Main.java
+++ Brouillon/src/Main.java
@@ -1,37 +1,66 @@
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		for(int n=100; n<=10000000; n*=10) {
-		System.out.println("Number of ints = "+n);
-		long bef, after;
-		DataGenerator gen = new DataGenerator(n);
-		gen.setUniform();
-		int ints[] = gen.getUniform(0.500);
-		//trying count of concise
-		int card = 0;
-		bef = System.nanoTime();
-		for(int i=0; i<100; i++){
-			for(int k=0; k<n; k++)
-				card+=count(ints[k]);
-		}
-		after = System.nanoTime();
-		
-		System.out.println("card = "+card+", Count concise time = "+((after-bef)/100)+" nanosec");
-		
-		//trying Integer.bitCount (popcnt)
-		 card = 0;
-		 bef = System.nanoTime();
-		 for(int i=0; i<100; i++){
-			 for(int k=0; k<n; k++)
-				card+=Integer.bitCount(ints[k]);
-			}
-				after = System.nanoTime();
-				
-				System.out.println("card = "+card+", Integer.bitCount(popcnt) time = "+((after-bef)/100)+" nanosec");
-		}
-	}
+    public static int summe1(int[] ints) {
+        int card = 0;
+        for(int k=0; k<ints.length; k++)
+            card+=count(ints[k]);
+        return card;
+    }
+
+    public static int summe2(int[] ints) {
+        int card = 0;
+        for(int k=0; k<ints.length; k++)
+            card+=Integer.bitCount(ints[k]);
+        return card;
+    }
+
+    public static void main(String[] args) {
+        // performing a dry-run
+        for(int n=100; n<=1000000; n*=10) {
+            System.out.println("Dry-run Number of ints = "+n);
+            DataGenerator gen = new DataGenerator(n);
+            gen.setUniform();
+            int ints[] = gen.getUniform(0.500);
+            int card = 0;
+            for(int i=0; i<100; i++) {
+                card += summe1(ints);
+            }
+            card = 0;
+            for(int i=0; i<100; i++) {
+                card += summe2(ints);
+            }
+
+        }
+        //launching the benchmarks
+        for(int n=100; n<=10000000; n*=10) {
+            System.out.println("Number of ints = "+n);
+            long bef, after;
+            DataGenerator gen = new DataGenerator(n);
+            gen.setUniform();
+            int ints[] = gen.getUniform(0.500);
+            //trying count of concise
+            int card = 0;
+            bef = System.nanoTime();
+            for(int i=0; i<100; i++) {
+                card += summe1(ints);
+            }
+            after = System.nanoTime();
+
+            System.out.println("card = "+card+", Count concise time = "+((after-bef)/100)+" nanosec");
+
+            //trying Integer.bitCount (popcnt)
+            card = 0;
+            bef = System.nanoTime();
+            for(int i=0; i<100; i++) {
+                card += summe2(ints);
+            }
+            after = System.nanoTime();
+
+            System.out.println("card = "+card+", Integer.bitCount(popcnt) time = "+((after-bef)/100)+" nanosec");
+
+        }
+    }
 	
 	/**
 	 * Population count
